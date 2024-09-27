b00560b22253d765dbe368350f77f7a12e56ae3d
zhongweijie95 <zhongweijie95@vip.qq.com>
2024-09-27 08:39:01 +0800

    Update routing.py
    Signed-off-by: zhongweijie95 <zhongweijie95@vip.qq.com>

--- flet_mvc/routing.py
+++ flet_mvc/routing.py
@@ -1,17 +1,34 @@
 import flet as ft
 
 
+# class RouteHandler:
+#     def __init__(self, page: ft.Page, fallback_content: list = None):
+#         self.page = page
+#         self.routes: dict[str, list] = {}
+#         self.fallback = fallback_content
+
+#     def register_route(self, route: str, view_content: list):
+#         self.routes[route] = view_content
+
+#     def route_change(self, e):
+#         self.page.views.clear()
+#         view_content = self.routes.get(e.route, self.fallback)
+#         self.page.views.append(ft.View(e.route, view_content))
+#         self.page.update()
+
 class RouteHandler:
     def __init__(self, page: ft.Page, fallback_content: list = None):
         self.page = page
-        self.routes: dict[str, list] = {}
+        self.routes: dict[str, ft.Control] = {}
         self.fallback = fallback_content
+        self._body = ft.Container(expand=True, padding=10)
+        self.page.add(self._body)
+        self.page.update()
 
     def register_route(self, route: str, view_content: list):
         self.routes[route] = view_content
 
     def route_change(self, e):
-        self.page.views.clear()
         view_content = self.routes.get(e.route, self.fallback)
-        self.page.views.append(ft.View(e.route, view_content))
-        self.page.update()
+        self._body.content = view_content
+        self._body.update()
